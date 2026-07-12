6656fe1f29522ae9ca488ec838b24db3d94d9718
pradeep8891 <pradeep.kumar5@rsystems.com>
2026-07-12 15:19:11 +0530

    update web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  devIndicators: false,
+
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
   /* config options here */
 };
 
